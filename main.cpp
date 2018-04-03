7965cf6ea984654e72b6fdef456c66650b5ef8a3
GustavoDLRA <A01410922@itesm.mx>
2018-04-03 00:17:53 -0500

    v2

--- main.cpp
+++ main.cpp
@@ -11,6 +11,12 @@ int main() {
     Dolar cantD5 = cantD1-cantP1;
     Peso cantP5=++cantP1;
     Dolar cantD6= --cantD1;
+    bool p1;
+    bool p2;
+    bool p3;
+    bool p4;
+    bool p5;
+    bool p6;
     std::cout<<"SISTEMA DE CAMBIO\n";
     std::cout<<"CANTIDAD DE DOLARES CON LAS QUE SE TRABAJA: ";
     std::cout<<cantD1.toString();
@@ -30,8 +36,49 @@ int main() {
     std::cout<<cantP5.toString();
     std::cout<<"Cant Dolares--\n";
     std::cout<<cantD6.toString();
-    
 
+
+    cantP1<cantD1? p2=true : p2=false;
+    std::cout<<"Monto en Pesos<Monto en Dolares\n";
+    std::cout<<cantP1.toString();
+    std::cout<<"<";
+    std::cout<<cantD1.toString()+"\n";
+    std::cout<<"Resultado="<<p2<<"\n";
+
+    cantP1>cantD1? p3=true : p3=false;
+    std::cout<<"Monto en Pesos>Monto en Dolares\n";
+    std::cout<<cantP1.toString();
+    std::cout<<">";
+    std::cout<<cantD1.toString()+"\n";
+    std::cout<<"Resultado="<<p3<<"\n";
+
+    cantP1<=cantD1? p1=true : p1=false;
+    std::cout<<"Monto en Pesos1<=Monto en Dolares\n";
+    std::cout<<cantP1.toString();
+    std::cout<<"<=";
+    std::cout<<cantD1.toString()+"\n";
+    std::cout<<"Resultado="<<p1<<"\n";
+
+    cantP1>=cantD1? p4=true : p4=false;
+    std::cout<<"Monto en Pesos>=Monto en Dolares\n";
+    std::cout<<cantP1.toString();
+    std::cout<<">=";
+    std::cout<<cantD1.toString()+"\n";
+    std::cout<<"Resultado="<<p4<<"\n";
+
+    cantP1==cantD1? p5=true : p5=false;
+    std::cout<<"Monto en Pesos1==Monto en Dolares\n";
+    std::cout<<cantP1.toString();
+    std::cout<<"==";
+    std::cout<<cantD1.toString()+"\n";
+    std::cout<<"Resultado="<<p5<<"\n";
+
+    cantP1!=cantD1? p6=true : p6=false;
+    std::cout<<"Monto en Pesos1!=Monto en Dolares\n";
+    std::cout<<cantP1.toString();
+    std::cout<<"!=";
+    std::cout<<cantD1.toString()+"\n";
+    std::cout<<"Resultado="<<p6<<"\n";
 
 
 
